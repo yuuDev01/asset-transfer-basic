2e1a8f9dc7d01f4accc28fd408e381fe0998d686
YuuDev01 <pasenoaz@gmail.com>
2023-11-26 20:20:23 +0000

    feat: Asset Read 기능 추가
    sever.js , assetread.ejs 코드 추가. 자산 조회 기능 구현

--- application/server.js
+++ application/server.js
@@ -21,16 +21,79 @@ app.get("/", (req,res)=>{
     res.render("index")
 })
 
-// get방식으로 url요청이 오면 다른 view 렌더링
-// 생성
-app.get("/asset/create", (req,res)=>{
-    res.render("assetcreate")
-})
-//조회
-app.get("/asset/read", (req,res)=>{
+app.get("/asset/read", (req, res)=>{
     res.render("assetread")
 })
+// 자산조회
+app.post("/asset/read", async (req,res)=>{
+    const id = req.body.ID
+    
+    console.log("in /asset/read")
+    console.log(id)
 
+    // 자산생성 수행
+    // 블록체인 네트워크에 있는 asset-transfer-basic 체인코드 호출
+    // 체인코드내에 있는 CreateAsset 함수를 호출해야함
+    try {
+        // load the network configuration
+        const ccpPath = path.resolve(__dirname, "ccp", "connection-org1.json");
+        let ccp = JSON.parse(fs.readFileSync(ccpPath, "utf8"));
+
+        // Create a new file system based wallet for managing identities.
+        const walletPath = path.join(process.cwd(), "wallet");
+        const wallet = await Wallets.newFileSystemWallet(walletPath);
+        console.log(`Wallet path: ${walletPath}`);
+
+        // Check to see if we've already enrolled the user.
+        const identity = await wallet.get("appUser");
+        if (!identity) {
+            console.log(
+                'An identity for the user "appUser" does not exist in the wallet'
+            );
+            console.log("Run the registerUser.js application before retrying");
+            return;
+        }
+
+        // Create a new gateway for connecting to our peer node.
+        const gateway = new Gateway();
+        await gateway.connect(ccp, {
+            wallet,
+            identity: "appUser",
+            discovery: { enabled: true, asLocalhost: true },
+        });
+
+        // Get the network (channel) our contract is deployed to.
+        const network = await gateway.getNetwork("mychannel");
+
+        // Get the contract from the network.
+        const contract = network.getContract("basic");
+
+        // Submit the specified transaction.
+        // createCar transaction - requires 5 argument, ex: ('createCar', 'CAR12', 'Honda', 'Accord', 'Black', 'Tom')
+        // changeCarOwner transaction - requires 2 args , ex: ('changeCarOwner', 'CAR12', 'Dave')
+        result = await contract.evaluateTransaction(
+            "ReadAsset",
+            id
+        );
+        console.log("Transaction has been submitted");
+        res_str = `{"resultcode":"success", "msg": ${result}}`
+        res.json(JSON.parse(res_str))
+
+        // Disconnect from the gateway.
+        await gateway.disconnect();
+    } catch (error) {
+        console.error(`Failed to submit transaction: ${error}`);
+        res_str = `{"resultcode":"failed", "msg":"자산조회 실패"}`
+        res.json(JSON.parse(res_str))
+    
+    }
+
+   
+})
+
+app.get("/asset/create", (req, res)=>{
+    res.render("assetcreate")
+})
 app.post("/asset/create", async (req,res)=>{
     const id = req.body.ID
     const color = req.body.COLOR
